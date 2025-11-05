0733f61866ecb48e307a11755defdb4adc00d57e
yesuuuss <jesupalma9@gmail.com>
2025-11-04 19:24:13 -0600

    first commit

---
+++ src/main/java/com/multipedidos/componente_a/ComponenteAApplication.java
@@ -0,0 +1,13 @@
+package com.multipedidos.componente_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponenteAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponenteAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multipedidos/componente_a/ComponenteAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multipedidos.componente_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponenteAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
