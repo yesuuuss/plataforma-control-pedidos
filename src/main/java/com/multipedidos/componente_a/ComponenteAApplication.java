86a04a4284957c5ae0573a6cd186e721e0dac7a6
yesuuuss <jesupalma9@gmail.com>
2025-11-13 17:39:43 -0600

    feat: se agregaron los componentes para el problema de cors

--- src/main/java/com/multipedidos/componente_a/ComponenteAApplication.java
+++ src/main/java/com/multipedidos/componente_a/ComponenteAApplication.java
@@ -2,15 +2,28 @@ package com.multipedidos.componente_a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.multipedidos.componente_a.model")
-@EnableJpaRepositories(basePackages = "com.multipedidos.componente_a.repository")  
 public class ComponenteAApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ComponenteAApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
